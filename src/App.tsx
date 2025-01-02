25ab92bfa90e1d50fda1c4a26e8d13b63fd5af50
adudaCH <johndoe@example.com>
2025-01-02 14:40:46 +0200

    2.1.25

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import MyCards from "./components/MyCards";
 import LikeButton from "./components/tools/LikeButton";
 import CardDetails from "./components/CardDetails";
 import { CardProvider } from "./contex/useCardContext";
+import SandBox from "./components/SandBox";
 
 function App() {
     const [theme, setTheme] = useState(() => {
@@ -45,11 +46,11 @@ function App() {
                         <Route path="/register" element={<Register />} />
                         <Route path="/" element={<Cards />} />
                         <Route path="/like-button" element={<LikeButton cardId={""} userId={""} />} />
-                        {/* <Route path="/profile" element={<Profile />} /> */}
+                        <Route path="/profile" element={<Profile />} />
                         <Route path="/card-details/:cardId" element={<CardDetails />} />
-                        {/* TODO: fix the component below */}
                         <Route path="/fav-cards" element={<FavCards />} />
                         <Route path="/my-cards" element={<MyCards />} />
+                        <Route path="/sand-box" element={<SandBox />} />
                         <Route path="*" element={<PageNotFound />} />
                     </Routes>
                 </Router>
